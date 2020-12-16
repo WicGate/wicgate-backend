224979ad8dea3fed4d2d6b1652cce1ad30b5760b
Sebastian Winkler <sebastian.winkler@yandex.com>
2020-12-16 22:22:46 +0100

    Initial django setup via cookiecutter

---
+++ wicgate/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from wicgate.users.models import User
+from wicgate.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
