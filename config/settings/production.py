94bbd75964d2a8d8c1a553bc6607006ddcdf98d5
Sebastian Winkler <sebastian.winkler@yandex.com>
2020-12-17 00:01:06 +0100

    Added WHITENOISE_MANIFEST_STRICT = False

--- config/settings/production.py
+++ config/settings/production.py
@@ -23,6 +23,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+WHITENOISE_MANIFEST_STRICT = False
 
 # GENERAL
 # ------------------------------------------------------------------------------
