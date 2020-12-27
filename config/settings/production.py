c6431aff20e612dda3722e44dbd52617ca7e8d09
Sebastian Winkler <sebastian.winkler@yandex.com>
2020-12-27 14:52:08 +0100

    production.py chore

--- config/settings/production.py
+++ config/settings/production.py
@@ -18,9 +18,10 @@ os.makedirs(STATIC_ROOT, exist_ok=True)
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+# python -c 'import secrets; print(secrets.token_urlsafe(38))'
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
 
 # DATABASES
 # ------------------------------------------------------------------------------
