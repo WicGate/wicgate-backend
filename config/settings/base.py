3840647cf1f7096adde4a68afad4c5d0885d915a
Sebastian Winkler <sebastian.winkler@yandex.com>
2020-12-27 02:46:13 +0100

    Possible fix for missing admin static files

--- config/settings/base.py
+++ config/settings/base.py
@@ -148,7 +148,7 @@ MIDDLEWARE = [
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-root
 STATIC_ROOT = str(ROOT_DIR / "staticfiles")
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-url
-STATIC_URL = "/static/"
+STATIC_URL = "/staticfiles/"
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [str(APPS_DIR / "static")]
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
